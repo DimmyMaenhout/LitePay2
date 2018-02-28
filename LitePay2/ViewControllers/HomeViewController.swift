489a14388a9ff306810f3095ef4c179613734b7b
DimmyMaenhout <dimmy.maenhout@telenet.be>
2018-02-28 15:30:58 +0100

    added ui & linked with controller

---
+++ LitePay2/ViewControllers/HomeViewController.swift
@@ -0,0 +1,9 @@
+import Foundation
+import UIKit
+
+class HomeViewController : UIViewController {
+    
+    
+    @IBOutlet weak var payBtn: UIButton!
+    @IBOutlet weak var receiveBtn: UIButton!
+}
