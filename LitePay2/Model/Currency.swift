f88b8a031415c5c4bd46182237db29c89d28609f
DimmyMaenhout <dimmy.maenhout@telenet.be>
2018-02-28 11:45:01 +0100

    added currency class

---
+++ LitePay2/Model/Currency.swift
@@ -0,0 +1,27 @@
+//
+//  Currency.swift
+//  LitePay2
+//
+//  Created by Dimmy Maenhout on 28/02/2018.
+//  Copyright © 2018 Dimmy Maenhout. All rights reserved.
+//
+
+import Foundation
+class Currency {
+    
+    var fullName = ""
+    var value : NSDecimalNumber = 0 /* gaat nog aangepast moeten worden naar BigDecimal! */
+    var code = ""
+    
+    convenience init(fullNameCurrency : String, valueCurrency : NSDecimalNumber, codeCurrency : String){
+        self.init()
+        self.fullName = fullNameCurrency
+        self.value = valueCurrency
+        self.code = codeCurrency
+    }
+}
+enum CurrencyCode {
+    /*Used for abbreviation of currency*/
+    case EUR
+    case LTC
+}
