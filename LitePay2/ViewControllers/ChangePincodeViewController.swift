ea7ed06c13bc5f6a7995626bd915ef7e6f6a074b
DimmyMaenhout <dimmy.maenhout@telenet.be>
2018-02-28 20:15:56 +0100

    added changePincodeScreen & linked controller

---
+++ LitePay2/ViewControllers/ChangePincodeViewController.swift
@@ -0,0 +1,9 @@
+import Foundation
+import UIKit
+
+class ChangePincodeViewController : UIViewController {
+    
+    @IBOutlet weak var currentPinTxtfield: UITextField!
+    @IBOutlet weak var newPinTxtField: UITextField!
+    @IBOutlet weak var repeatNewPinTxtfield: UITextField!
+}
