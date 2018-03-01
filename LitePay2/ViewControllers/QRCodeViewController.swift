ec014fd9aeecab25a2ee401aa0b5ce8e81691d65
DimmyMaenhout <dimmy.maenhout@telenet.be>
2018-03-01 09:46:40 +0100

    added UI_receiveScreen & linked with controller

---
+++ LitePay2/ViewControllers/QRCodeViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ReceiveViewController.swift
+//  LitePay2
+//
+//  Created by Dimmy Maenhout on 01/03/2018.
+//  Copyright © 2018 Dimmy Maenhout. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class QRCodeViewController : UIViewController {
+    
+    @IBOutlet weak var QRImageView : UIImageView!
+}
