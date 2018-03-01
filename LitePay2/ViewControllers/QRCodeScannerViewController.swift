a30b3a939b50439809209a61275281871b715691
DimmyMaenhout <dimmy.maenhout@telenet.be>
2018-03-01 10:21:27 +0100

    added ScanQRCodeScreen & linked controller

---
+++ LitePay2/ViewControllers/QRCodeScannerViewController.swift
@@ -0,0 +1,16 @@
+//
+//  QRCodeScannerViewController.swift
+//  LitePay2
+//
+//  Created by Dimmy Maenhout on 01/03/2018.
+//  Copyright © 2018 Dimmy Maenhout. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import AVFoundation
+
+class QRCodeScannerViewController : UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+    
+    @IBOutlet weak var QRCodeScannerImageView : UIImageView!
+}
