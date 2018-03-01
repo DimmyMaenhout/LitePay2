2031e9a7fd060cf2464ea0f1771ac35ce85ad8e9
DimmyMaenhout <dimmy.maenhout@telenet.be>
2018-03-01 08:46:21 +0100

    added loginScreen & linked controller

---
+++ LitePay2/ViewControllers/LoginViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.swift
+//  LitePay2
+//
+//  Created by Dimmy Maenhout on 01/03/2018.
+//  Copyright © 2018 Dimmy Maenhout. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class LoginViewController : UIViewController {
+    
+    @IBOutlet weak var emailTxtField : UITextField!
+    @IBOutlet weak var passwordTxtField : UITextField!
+    @IBOutlet weak var loginBtn : UIButton!
+    
+}
