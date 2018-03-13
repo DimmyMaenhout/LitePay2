3a4b5a943259e8454425633f2c99f62d194971de
Dimmy Maenhout <DimmyMaenhout@MacBook-Pro-van-Dimmy.local>
2018-03-13 13:16:04 +0100

    cleaned up code

--- LitePay2/Services/CoinbaseAPI.swift
+++ LitePay2/Services/CoinbaseAPI.swift
@@ -5,7 +5,9 @@ public enum CoinbaseAPI {
     
     //Redirects to coinbase so user can login with coinbase account
     public static func redirectOauth() {
+        
         CoinbaseOAuth.startAuthentication(withClientId: "\(LitePayData.clientId)", scope: "user balance", redirectUri: "\(LitePayData.redirectUriTest)", meta: nil)
+    
     }
 
 }
