d4377c10686fb1769780306504012a99334e66b8
Dimmy Maenhout <DimmyMaenhout@MacBook-Pro-van-Dimmy.local>
2018-03-13 12:05:59 +0100

    Cleaned code && moved constants to LitePayData

---
+++ LitePay2/Services/LitePayData.swift
@@ -0,0 +1,19 @@
+//
+//  LitePayData.swift
+//  LitePay2
+//
+//  Created by Dimmy Maenhout on 08/03/2018.
+//  Copyright © 2018 Dimmy Maenhout. All rights reserved.
+//
+
+import Foundation
+struct LitePayData {
+    
+    static let clientId = "e2598b8ec89426575829e0f50c0a3ea827ffdfbabd205ecf9a871bfcb0386684"
+    static let clientSecret = "7647dd6a664ef3b65739afcb27adee74da0a09ff9fd96510cbee27343ee8fd16"
+    static let redirectUri = "urn:ietf:wg:oauth:2.0:oob"
+    static let redirectUriTest = "dmaenhout.litepay2://coinbase-oauth"
+    
+    /* mock */
+    static let baseUrlMockAPI = "https://gateway.api.cloud.wso2.com:443/t/xg6106/cb4/1"
+}
