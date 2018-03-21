483c684de1264f2f62b8cca9c2af223a245f3974
Dimmy Maenhout <DimmyMaenhout@mbp-van-dimmy.iconos.be>
2018-03-21 15:09:20 +0100

    changed scopes to v1 (API)

--- LitePay2/Services/CoinbaseAPIService.swift
+++ LitePay2/Services/CoinbaseAPIService.swift
@@ -7,8 +7,13 @@ public enum CoinbaseAPIService {
     
     //Redirects to coinbase so user can login with coinbase account
     public static func startOAuth() {
-        //3rd parameter for meta: AnyHashable("send_limit_currency"):"LTC",
-        CoinbaseOAuth.startAuthentication(withClientId: LitePayData.clientId, scope: "wallet:accounts:read, wallet:accounts:update, wallet:accounts:create, wallet:addresses:read, wallet:addresses:create, wallet:transactions:read, wallet:transactions:send, wallet:user:email", redirectUri: LitePayData.redirectUriTest, meta: [AnyHashable("send_limit_amount"): "0.0056", AnyHashable("send_limit_currency"): "EUR", AnyHashable("send_limit_period"): "day" ])
+        //v2 : "wallet:accounts:read, wallet:addresses:read, wallet:addresses:create, wallet:transactions:read, wallet:transactions:send, wallet:user:email"
+        CoinbaseOAuth.startAuthentication(withClientId: LitePayData.clientId,
+                                          scope: "balance transactions user send orders",
+                                          redirectUri: LitePayData.redirectUriTest,
+                                          
+                                          //send_limit_amount: A limit to the amount of money your application can send from the user’s account. This will be displayed on the authorize screen
+            meta: [AnyHashable("send_limit_amount"): "0.8098", AnyHashable("send_limit_currency"): "EUR", AnyHashable("send_limit_period"): "day"])
     }
-
+    //scopes v1: balance transactions user send orders
 }
