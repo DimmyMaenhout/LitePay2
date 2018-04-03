006af50aa1838e8f7dac5648e159885df23b05cd
Dimmy Maenhout <DimmyMaenhout@mbp-van-dimmy.iconos.be>
2018-04-03 10:37:30 +0200

    Added check to see if textfields empty

--- LitePay2/ViewControllers/PayViewController.swift
+++ LitePay2/ViewControllers/PayViewController.swift
@@ -1,5 +1,6 @@
 import Foundation
 import UIKit
+import coinbase_official
 
 class PayViewController : UIViewController {
     
@@ -7,4 +8,16 @@ class PayViewController : UIViewController {
     @IBOutlet weak var LtcTxtField: UITextField!
     @IBOutlet weak var euroTxtField: UITextField!
     @IBOutlet weak var ScanQRBtn: UIButton!
+    
+    var account : CoinbaseAccount!
+    
+    override func viewDidLoad() {
+        //checkFieldEmpty()
+    }
+    func checkFieldEmpty(){
+       
+        if  LtcTxtField.text == "" && euroTxtField.text == "" || euroTxtField.text == "" && euroTxtField.text == "" {
+            ScanQRBtn.isEnabled = false
+        }
+    }
 }
