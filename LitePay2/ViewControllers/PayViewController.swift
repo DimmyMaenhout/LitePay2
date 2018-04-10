55f89d22ef166f96c301c3d8a795ec446ac6b804
DimmyMaenhout <dimmy.maenhout@telenet.be>
2018-04-10 15:41:48 +0200

    Controller finished

--- LitePay2/ViewControllers/PayViewController.swift
+++ LitePay2/ViewControllers/PayViewController.swift
@@ -19,50 +19,54 @@ class PayViewController : UIViewController {
         }
     }
     
-    var activeTextField : UITextField!
+    var spotPriceEuro : Decimal? {
+        didSet {
+            calculate()
+        }
+    }
+    
+//    var activeTextField : UITextField!
     var i = 0
+    var sufficientBalance = false
+    
     override func viewDidLoad() {
         
         super.viewDidLoad()
-//        checkFieldEmpty()
-//        CoinbaseAPIService.getExchangeRates(for: account.balance.currency)
         getValueForCurrency()
-        
+    
 //        sets the euro textfield as "active" (ready for the user to type when he/she comes on the screen)
         euroTxtField.becomeFirstResponder()
         ScanQRBtn.layer.cornerRadius = 5
         ltcTxtField.isEnabled = false
-        
+        ScanQRBtn.isEnabled = false
         addDoneButtonOnKeyboard()
-    }
-    
-//    When the user taps an area on the screen, this method is called (not when tapping keyboard)
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        changeButtonColor()
+        changeButtonIsEnabled()
         
-//        when the keyboard is showing and the user taps somewhere on the screen that is not the keyboard, the keyboard will dissapear
-        self.view.endEditing(true)
+        getValueInEuro()
     }
     
 //    switches the textfields from place when switch button is pressed
     @IBAction func switchBtnPressed(_ sender: Any) {
         
         print("Payview controller line 47, in function switchBtnPressed, i = \(i)")
+        
         UIView.beginAnimations(nil, context: nil)
         
         if (i % 2 != 0) {
-            
+//            i is odd
             print("Payview controller line 52, in if statement. i = \(i)")
             (euroTxtField.frame.origin, ltcTxtField.frame.origin, euroTxtField.placeholder, ltcTxtField.placeholder, euroTxtField.frame.size.width) =
             (ltcTxtField.frame.origin, euroTxtField.frame.origin, ltcTxtField.placeholder, euroTxtField.placeholder, ltcTxtField.frame.size.width)
             
             (euroIconLbl.frame.origin, ltcIconLbl.frame.origin, euroIconLbl.text, ltcIconLbl.text) =
-            (ltcIconLbl.frame.origin, euroIconLbl.frame.origin, ltcIconLbl.text, euroTxtField.text)
+            (ltcIconLbl.frame.origin, euroIconLbl.frame.origin, ltcIconLbl.text, euroIconLbl.text)
             
-            print("Payview controller line 59, ltcTextField.isEnabled = \(ltcTxtField.isEnabled)")
             print("Payview controller line 60, euroTextField.isEnabled = \(euroTxtField.isEnabled)")
+            print("Payview controller line 59, ltcTextField.isEnabled = \(ltcTxtField.isEnabled)")
         }
         else {
-            
+//            i is even
             print("Payview controller line 64, in else statement. i = \(i)")
             (ltcTxtField.frame.origin, euroTxtField.frame.origin, ltcTxtField.placeholder, euroTxtField.placeholder, ltcTxtField.frame.size.width) =
             (euroTxtField.frame.origin, ltcTxtField.frame.origin, euroTxtField.placeholder, ltcTxtField.placeholder, euroTxtField.frame.size.width)
@@ -73,117 +77,177 @@ class PayViewController : UIViewController {
             print("Payview controller line 71, ltcTextField.isEnabled = \(ltcTxtField.isEnabled)")
             print("Payview controller line 72, euroTextField.isEnabled = \(euroTxtField.isEnabled)")
         }
+        
         UIView.commitAnimations()
         i += 1
     }
     
     //    If editing stopped the value wil be conversed to another currnency (depends on acount.balance.currency)
     @IBAction func euroTextfieldDidChange(_ sender: UITextField) {
+        
         print("Pay view controller line 80, euro textfield: \(String(describing: euroTxtField.text))")
-        var input = euroTxtField.text
+        var input = NSDecimalNumber(string: euroTxtField.text)
+        var cur = CurrencyRate()
+        cur.converseToLTC(amountEuro: input, currencyRate: rate as! NSDecimalNumber)
+        print()
+        checkFieldEmpty()
+        calculate()
         checkSufficientBalance()
-    }
-    
-//    if editing stopped the value wil be conversed to euro
-//    @IBAction func ltcTextfieldDidChange(_ sender: Any) {
-//        print("Pay view controller line 33, ltc textfield: \(String(describing: ltcTxtField.text))")
-//        
-//    }
-    
-    func euroTextfieldDidBeginEditing(_ textfield: UITextField) {
-        //move
     }
 
     func getValueForCurrency() {
     
         CoinbaseAPIService.getExchangeRateFor(currency: account.balance.currency, completion: { response in
             
-            print("Pay view controller line 34, response: \(String(describing: response))")
+            print("Pay view controller line 101, response: \(String(describing: response))")
             guard let cur = response else {
                 
                 return
             }
-            print("Pay view controller line 45, cur: \(String(describing: cur))")
+            print("Pay view controller line 106, cur: \(String(describing: cur))")
             
             guard let value = cur[self.account.balance.currency] else {
                 
-                print("Pay view controller line 52, value is nil")
+                print("Pay view controller line 110, value is nil")
                 return
             }
             
-            print("Pay view controller line 54, value: \(String(describing: value))")
+            print("Pay view controller line 114, value: \(String(describing: value))")
             
             self.rate = NSDecimalNumber(string: value) as Decimal
-            print("Pay view controller line 63, rate: \(String(describing: self.rate))")
+            print("Pay view controller line 117, rate: \(String(describing: self.rate))")
+        })
+    }
+    
+    func getValueInEuro(){
+        CoinbaseAPIService.getSpotPrice(currencyAccount: account.balance.currency, completion: {response in
+            
+            print("Pay view controller line 118, response: \(String(describing: response))")
+            guard let value = response else {
+                
+                return
+            }
+            print("Pay view controller line 129, value €: \(String(describing: value))")
+            
+            self.spotPriceEuro = NSDecimalNumber(string: response) as Decimal
+            print("Pay view controller line 132, spotPriceEuro: \(String(describing: self.spotPriceEuro))")
         })
     }
     
     func calculate() {
         
-        print("Pay view controller line 69, in func calculate")
-        print("Pay view controller line 70, rate: \(String(describing: self.rate))")
-        if euroTxtField.text != "" {
+        print("Pay view controller line 138, in func calculate")
+        print("Pay view controller line 139, rate: \(String(describing: self.rate))")
+        var euroField = NSDecimalNumber(string: euroTxtField.text) as Decimal
         
-            var valueTextField = NSDecimalNumber(string: euroTxtField.text)
+        if i % 2 == 0 {
+            
+//            calculate € to other currency
             guard let rateValue = self.rate else {
-                print("Pay view controller line 75, rateValue is nil")
+                
+                print("Pay view controller line 147, rateValue is nil")
                 return
             }
-            let value = 1 / rateValue
-            print("Pay view controller line 79, value: \(String(describing: value))")
+            
+            let amount = euroField * rateValue
+            let decimalValue = amount
+            ltcTxtField.text = decimalValue.description
+            print("Pay view controller line 154, ltcTxtField = \(ltcTxtField.text)")
+        }
+        else {
+            
+            guard var spotPrice = spotPriceEuro else {
+                print("Pay view controller line 159, rateValue is nil")
+                return
+            }
+            let amount = spotPrice * euroField
+            let decimalValue = amount
+            ltcTxtField.text = decimalValue.description
+            print("Pay view controller line 165, ltcTxtField = \(ltcTxtField.text)")
         }
     }
     
 //    If 2 fields are empty, it's not possible to scan an account address (button disabled)
     func checkFieldEmpty() {
        
-        if  ltcTxtField.text == "" && euroTxtField.text == "" {
+        if  euroTxtField.text == "" {
             
-            ScanQRBtn.isEnabled = false
+            changeButtonIsEnabled()
         }
-        print("Pay view controller line 90, coinbaseAccount from previous controller (Select account view controller): \nAccountID: \(account.accountID) \nBalance: \(account.balance.amount)")
+        print("Pay view controller line 176, coinbaseAccount from previous controller (Select account view controller): \nAccountID: \(account.accountID) \nBalance: \(account.balance.amount)")
     }
     
 //    Check if sufficient funds
     func checkSufficientBalance() {
         
         if !(euroTxtField.text?.isEmpty)! {
-            print("Pay view controller line 149, coinbaseAccount from previous controller (Select account view controller): \nAccountID: \(account.accountID) \nBalance: \(account.balance.amount)")
-            var cur = CurrencyRate()
+            print("Pay view controller line 183, coinbaseAccount from previous controller (Select account view controller): \nAccountID: \(account.accountID) \nBalance: \(account.balance.amount)")
+            
             let amount = NSDecimalNumber(string: euroTxtField.text)
             let balance = NSDecimalNumber(string: account.balance.amount)
             
-            
+            let cur = CurrencyRate()
             var amountInWalletCurrency = cur.converseToLTC(amountEuro: amount, currencyRate: rate as! NSDecimalNumber)
+            
             if amountInWalletCurrency.decimalValue > balance.decimalValue {
                 
                 let alert = UIAlertController(title: "", message: "Het ingegeven bedrag is ontoereikend", preferredStyle: .alert)
                 alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
                 self.present(alert, animated: true)
                 
+                sufficientBalance = false
+//                calculate()
+                changeButtonColor()
+                changeButtonIsEnabled()
             }
-            
-            //cur.converseToLTC(amountEuro: <#T##NSDecimalNumber#>, currencyRate: <#T##NSDecimalNumber#>)
+            else {
+                sufficientBalance = true
+//                calculate()
+                changeButtonColor()
+                changeButtonIsEnabled()
+            }
         }
-//        if !(ltcTxtField.text?.isEmpty)! {
-//            
-//            
-//        }
     }
+    
+    func changeButtonIsEnabled() {
+        
+        if sufficientBalance == false {
+            
+            ScanQRBtn.isEnabled = false
+        }
+        else {
+            
+            ScanQRBtn.isEnabled = true
+        }
+    }
+    
+    func changeButtonColor(){
+        if sufficientBalance == false {
+            
+            ScanQRBtn.backgroundColor = UIColor(red: 0.722, green: 0.722, blue: 0.722, alpha: 0.45)
+        }
+        else {
+            
+            ScanQRBtn.backgroundColor = UIColor(red: 0.000, green: 0.479, blue: 0.999, alpha: 1)
+        }
+    }
+    
 //    Adds a done button above the keyboard (decimal pad)
     func addDoneButtonOnKeyboard() {
         
         let doneToolbar : UIToolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: 320, height: 50))
         doneToolbar.barStyle = UIBarStyle.blackTranslucent
         
+//        Sets the button on the right in the toolbar
         var flexSpace = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.flexibleSpace, target: nil, action: nil)
         let doneButton: UIBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.done, target: self, action: #selector(self.doneButtonAction))
         
-        var items = NSMutableArray()
-
         doneToolbar.setItems([flexSpace, doneButton], animated: false)
+        
+//        "resizes the current view so that it uses the most appropriate amount of space"
         doneToolbar.sizeToFit()
         
+//        "this property is used to attach an accessory view to the system-supplied keyboard, that is represented for the UITextField and UITextView objects"
         euroTxtField.inputAccessoryView = doneToolbar
     }
     
@@ -202,7 +266,7 @@ extension PayViewController : UITextFieldDelegate {
         
 //      "lift" our scrollview 150
         scrollView.setContentOffset(CGPoint(x: 0, y: 150), animated: true)
-        activeTextField = textField
+//        activeTextField = textField
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
