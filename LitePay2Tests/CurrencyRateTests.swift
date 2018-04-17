7c17a584626aef45b98cbd9680296a5c57a95b8c
DimmyMaenhout <dimmy.maenhout@telenet.be>
2018-04-17 14:32:43 +0200

    Added unit tests (file)

---
+++ LitePay2Tests/CurrencyRateTests.swift
@@ -0,0 +1,44 @@
+import XCTest
+//@testable import LitePay2
+//import Alamofire
+//import coinbase_official
+
+class CurrencyRateTests: XCTestCase {
+    
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+//    func converseToLTC(amountEuro: NSDecimalNumber, currencyRate: NSDecimalNumber) -> NSDecimalNumber {
+//
+//        let amountLTC = (amountEuro as Decimal) * (currencyRate as Decimal) as NSDecimalNumber
+//        return amountLTC
+//    }
+//
+//    func converseToEuro(amountLTC: NSDecimalNumber, spotPrice: NSDecimalNumber) -> NSDecimalNumber {
+//
+//        let amountEuro = (amountLTC as Decimal) * (spotPrice as Decimal) as NSDecimalNumber
+//        return amountEuro
+//    }
+    //          Testen
+    //    1. amountEuro of currencyRate is 0
+    //    2. amountEuro of currencyRate is negatief
+    //    3. amountLtc of currencyRate is negatief
+    //    4. amount of currrencyRate is geen getal
+    
+    func testCurrencyRateIsZero(amountEuro: NSDecimalNumber, currencyRate: NSDecimalNumber) {
+        
+//        let cur : CurrencyRate  // = CurrencyRate()
+////        curr.
+//        let currency : Currency
+//        XCTAssert(<#T##expression: Bool##Bool#>, <#T##message: String##String#>)
+    }
+    
+}
