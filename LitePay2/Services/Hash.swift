5c1633d50a06ac7be121d2f97e27c8b499defa92
DimmyMaenhout <dimmy.maenhout@telenet.be>
2018-05-23 11:04:37 +0200

    Added struct Hash

---
+++ LitePay2/Services/Hash.swift
@@ -0,0 +1,12 @@
+//
+//  Hash.swift
+//  LitePay2
+//
+//  Created by Dimmy Maenhout on 23/05/2018.
+//  Copyright © 2018 Dimmy Maenhout. All rights reserved.
+//
+
+import Foundation
+struct Hash {
+    public static let salty = "g48Fa4faQaRfeVNFjliwTdsw§{0;fsaQAEPFRdP"
+}
